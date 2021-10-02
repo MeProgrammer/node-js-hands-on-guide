e3b1b74add6b086864a8f1e851fc7eb868f27d52
Mohamed Mousa <bahnawi@yahoo.com>
2021-10-02 20:14:25 +0200

    object and string

---
+++ Strings.js
@@ -0,0 +1,9 @@
+// create a list of us states
+const myName = 'Mohamed Mousa'
+
+console.log(myName.split(' '))
+console.log(myName.charAt(4))
+console.log(myName.indexOf('ou', 10))
+
+console.log(myName.replace('ou', 'o'))
+
---
+++ objects.js
@@ -0,0 +1,8 @@
+user = {
+    first_name: 'marc',
+    last_name: 'last',
+    age: Infinity,
+    citizenship: "man of the world"
+}
+
+console.log(Object.keys(user).length)
